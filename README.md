78e1b925fabbe17fbf2b4d2dde78687adb9ef46a
Steven Enten <steven@enten.fr>
2016-01-10 01:59:12 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,42 @@
+# node-mw
+
+>  Middleware factory
+
+## Install
+
+```bash
+npm install node-mw --save
+```
+
+## Usage
+
+```javascript
+var mwFactory = require('node-mw');
+
+mwFactory({foo:'bar'})
+  .use(function (a, next, end) {
+    console.log('CP1', this, arguments);
+    next(null, 'X');
+  })
+  .use(function (x, next, end) {
+    console.log('CP2', this, arguments);
+    next(undefined, 'Y');
+  })
+  .use(function (y, next, end) {
+    console.log('CP3', this, arguments);
+    next(null, 'Z')
+  })
+  .run('a', function () {
+    console.log('END', this, arguments);
+  });
+
+/** output **/
+// CP1 { foo: 'bar' } { '0': 'a', '1': [Function: bound ], '2': [Function: bound ] }
+// CP2 { foo: 'bar' } { '0': 'X', '1': [Function: bound ], '2': [Function: bound ] }
+// CP3 { foo: 'bar' } { '0': 'Y', '1': [Function: bound ], '2': [Function: bound ] }
+// END { foo: 'bar' } { '0': null, '1': 'Z' }
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,125 @@
+'use strict';
+
+module.exports = MiddlewareFactory;
+module.exports.Middleware = Middleware;
+module.exports.MiddlewareProcess = MiddlewareProcess;
+
+function MiddlewareFactory() {
+  var args, mw;
+  args = slice(arguments);
+  mw = new Middleware();
+  if (args.length && !isArrayOrFunction(args[args.length-1])) {
+    mw.context = args.pop();
+  }
+  if (args.length) {
+    mw.use.apply(mw, args);
+  }
+  return mw;
+}
+
+function Middleware() {
+  this.context = {};
+  this.stack = [];
+}
+
+Middleware.prototype = {
+  use: function () {
+    var args = slice(arguments);
+    while (args.length) {
+      if (isArray(args[0])) {
+        this.use.apply(this, args.shift());
+        continue;
+      }
+      if (!isFunction(args[0])) {
+        throw new TypeError();
+      }
+      this.stack.push(args.shift());
+    }
+    return this;
+  },
+  run: function () {
+    var mw, args, done;
+    mw = deepClone(this);
+    args = slice(arguments);
+    if (isFunction(args[args.length-1])) {
+      done = args.pop();
+    }
+    setTimeout(function () {
+      new MiddlewareProcess(mw, done).exec(args);
+    }, 0)
+  }
+};
+
+function MiddlewareProcess(mw, done) {
+  this.mw = mw;
+  this.done = done;
+}
+
+MiddlewareProcess.prototype = {
+  exec: function (args) {
+    if (!this.mw.stack.length) {
+      end();
+    } else {
+      args.push(this.next.bind(this));
+      args.push(this.end.bind(this));
+      try {
+        this.mw.stack.shift().apply(this.mw.context, args);
+      } catch (err) {
+        this.end(err);
+      }
+    }
+  },
+  end: function () {
+    if (this.done) {
+      this.done.apply(this.mw.context, slice(arguments));
+    }
+  },
+  next: function (err) {
+    if (err || !this.mw.stack.length) {
+      this.end.apply(this, slice(arguments));
+      return;
+    }
+    this.exec(slice(arguments, 1));
+  }
+};
+
+function deepClone(obj) {
+  var copy, i;
+  if (!obj || !isObject(obj)) {
+    return obj;
+  }
+  if (isArray(obj)) {
+    copy = [];
+    for (i = 0; i < obj.length; ++i) {
+      copy[i] = deepClone(obj[i]);
+    }
+    return copy;
+  }
+  copy = {};
+  for (i in obj) {
+    if (obj.hasOwnProperty(i)) {
+      copy[i] = deepClone(obj[i]);
+    }
+  }
+  return copy;
+}
+
+function isArray(obj) {
+  return Array.isArray(obj);
+}
+
+function isArrayOrFunction(obj) {
+  return isArray(obj) || isFunction(obj);
+}
+
+function isFunction(obj) {
+  return typeof obj === 'function';
+}
+
+function isObject(obj) {
+  return typeof obj === 'object';
+}
+
+function slice(obj, start, end) {
+  return Array.prototype.slice.call(obj, start, end);
+}
